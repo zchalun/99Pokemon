01f2d95296ab49113c1a4ed482ca67205a45449f
grace <grace@Grace-HP>
2020-05-02 20:21:23 -0700

    Added logo

--- ui_control/MainMenu.java
+++ ui_control/MainMenu.java
@@ -2,7 +2,11 @@ package ui_control;
 import java.awt.Color;
 import java.awt.EventQueue;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
 
 public class MainMenu extends JFrame {
 
@@ -33,8 +37,23 @@ public class MainMenu extends JFrame {
 	 * Create the frame.
 	 */
 	public MainMenu() {
-		getContentPane().setBackground(new Color(204, 255, 255));
+		getContentPane().setBackground(Color.BLACK);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(0, 0, 1200, 750);
+		getContentPane().setLayout(null);
+		
+		JPanel panel = new JPanel();
+		getContentPane().add(panel);
+		panel.setBounds(0, 0, 1200, 94);
+		panel.setBackground(new Color(36,104,177));
+		
+		JLabel logo = new JLabel("");
+		java.awt.Image logoimage = new ImageIcon("99-Pok.png").getImage();
+		panel.setLayout(null);
+		panel.add(logo); 
+		logo.setForeground(Color.WHITE);
+		logo.setIcon(new ImageIcon(logoimage));
+		logo.setBounds(0, 0, 240, 94);
+		
 	}
 }
