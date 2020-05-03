2702be32daf7ab69544b6afc344560a735adea01
grace <grace@Grace-HP>
2020-05-02 19:51:11 -0700

    Created MainMenu frame

--- ui_control/MainMenu.java
+++ ui_control/MainMenu.java
@@ -1,6 +1,40 @@
 package ui_control;
+import java.awt.Color;
+import java.awt.EventQueue;
+
 import javax.swing.JFrame;
 
-public class MainMenu {
+public class MainMenu extends JFrame {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	//private final Action action = new SwingAction();
+
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MainMenu frame = new MainMenu();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public MainMenu() {
+		getContentPane().setBackground(new Color(204, 255, 255));
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(0, 0, 1200, 750);
+	}
 }
