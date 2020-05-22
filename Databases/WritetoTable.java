667be104175ff5f42567e574010f9743e2d95c92
CynthiaZ <CynthiaZ@LAPTOP-4AQTJA6D>
2020-05-21 20:08:54 -0700

    there's a database,,, have to figure out how to make it relevant

---
+++ Databases/WritetoTable.java
@@ -0,0 +1,56 @@
+package Databases;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.document.DynamoDB;
+import com.amazonaws.services.dynamodbv2.document.Item;
+import com.amazonaws.services.dynamodbv2.document.PutItemOutcome;
+import com.amazonaws.services.dynamodbv2.document.Table;
+
+public class WritetoTable {
+
+    public static void main(String[] args) throws Exception {
+    	//connect();
+    	//displayAll();
+    	create();
+    }
+    public static Connection connect() {
+		String url = "jdbc:sqlite:amazon2.db"; //url tells the driver manager where to locate the Database.db file
+		Connection conn = null;
+		
+		//Attempts to connect to the database file
+		try {
+			conn = DriverManager.getConnection(url);
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
+		return conn; //returns a connection object
+	}
+	
+	
+	public static void create() throws InterruptedException, SQLException
+	{
+		AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard().withRegion("us-west-2").build();
+		DynamoDB dynamoDB = new DynamoDB(client);
+
+		Table table = dynamoDB.getTable("Reservations");
+		
+		// Build the item
+		Item item = new Item()
+		    .withPrimaryKey("name", "Gao","password","password2@")
+		    .withNumber("numGuests", 4)
+		    .withString("time", "12:45 PM");
+
+		// Write the item to the table
+		PutItemOutcome outcome = table.putItem(item);
+	}
+}
