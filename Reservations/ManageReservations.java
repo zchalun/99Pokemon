5961130e9ad4e78bb90f8c826b44b2cbbc1e7bed
CynthiaZ <CynthiaZ@LAPTOP-4AQTJA6D>
2020-05-21 20:34:42 -0700

    search reservations now works, but the return is ugly

--- Reservations/ManageReservations.java
+++ Reservations/ManageReservations.java
@@ -1,6 +1,16 @@
 package Reservations;
 import java.sql.SQLException;
 import java.util.*;
+
+import com.amazonaws.AmazonClientException;
+import com.amazonaws.auth.profile.ProfileCredentialsProvider;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.document.DynamoDB;
+import com.amazonaws.services.dynamodbv2.document.Item;
+import com.amazonaws.services.dynamodbv2.document.Table;
+import com.amazonaws.services.dynamodbv2.model.ScanRequest;
+import com.amazonaws.services.dynamodbv2.model.ScanResult;
 public class ManageReservations
 {
 	public ArrayList<Reservation> Reservations = new ArrayList<Reservation>();
@@ -35,24 +45,41 @@ public class ManageReservations
 	 */
     public String viewReservations(String n, String a)
     {
-    	//Reservations.add(addReservations("Zhang",3,"12:30 PM","password")); //sample Reservation entry for testing
-    	/*for (Reservation x : Reservations)
-    	{
-    		System.out.println(x.toString());
-    	}*/
-    	for (Reservation x : Reservations)
-    	{
-    		if (x.getName().contentEquals(n) && x.getAccessPassword().contentEquals(a))
-    		{
-    			return x.toString();
-    		}
-    		else if (x.getName().contentEquals(n) && !x.getAccessPassword().contentEquals(a))
-    		{
-    			return "incorrect access password";
-    		}
-    	}
-    	return "no reservation found. make one?";
-    }
+		ProfileCredentialsProvider credentialsProvider = new ProfileCredentialsProvider();
+        try {
+            credentialsProvider.getCredentials();
+        } catch (Exception e) {
+            throw new AmazonClientException(
+                    "Cannot load the credentials from the credential profiles file. " +
+                    "Please make sure that your credentials file is at the correct " +
+                    "location (/Users/johnmortensen/.aws/credentials), and is in valid format.",
+                    e);
+        }
+        AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard()
+        	.withCredentials(credentialsProvider)
+            .withRegion("us-west-2")
+            .build();
+
+		DynamoDB dynamoDB = new DynamoDB(client);
+	    Table table = dynamoDB.getTable("Reservations");
+	    ScanRequest scanRequest = new ScanRequest()
+	    	    .withTableName("Reservations");
+
+
+	    ScanResult result = client.scan(scanRequest);
+	    Item item = null;
+
+	    for(int x = 1; x <= result.getCount(); x++)
+	    {
+	    	item = table.getItem("name", n, "password", a);
+	    }
+	    if (item == null)
+	    {
+	    	return "reservation not found";
+	    }
+
+		return item.toJSONPretty();
+	}
     
     public static String driverView(String n, String a)
     {
@@ -65,10 +92,8 @@ public class ManageReservations
      * @param a access password
      * @return whether reservation was successfully removed
      */
-    public String deleteReservation(String n, String a)
+    /*public String deleteReservation(String n, String a)
     {
-    	//Reservations.add(addReservations("Zhang",3,"12:30 PM","password")); //sample Reservation entry for testing
-
     	for (int i = 0; i<Reservations.size(); i++)
     	{
     		Reservation x = Reservations.get(i);
@@ -82,13 +107,14 @@ public class ManageReservations
     			return "incorrect access password";
     		}
     	}
-    	return "no reservation found.";
+    	return "no reservation found.";   		
+    	
     }
     public static String driverDelete(String n, String a)
     {
     	ManageReservations temp = new ManageReservations();
     	return temp.deleteReservation(n,a);
-    }
+    }*/
     
     /**
      * @param n name reservation is under
